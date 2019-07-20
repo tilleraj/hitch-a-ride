782e5aa7e2a07e57c08ea360cc7b5db9c5f738c3
Andrew Tiller <tiller.andrew@gmail.com>
2019-07-20 12:26:25 -0500

    setup complete. installed node-sass, eslint-config-airbnb-base, bootstrap, and firebase. bootstrap tested to be working

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <button className="btn btn-danger">Bootstrap Test</button>
+      </div>
+    );
+  }
+}
+
+export default App;
