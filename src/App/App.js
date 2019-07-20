4e268a05018cadb98c1475360d0d082ff04a783c
Andrew Tiller <tiller.andrew@gmail.com>
2019-07-20 13:04:31 -0500

    firebase auth through google account is working NavBar is displaying and has logout

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,49 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import firebase from 'firebase/app';
+
+import Auth from '../components/Auth/Auth';
+import Home from '../components/Home/Home';
+import NavBar from '../components/NavBar/NavBar';
+import fbConnection from '../helpers/data/connection';
+
 import './App.scss';
 
+fbConnection();
+
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+    const loadComponent = () => {
+      if (authed) {
+        return <Home />;
+      }
+      return <Auth />;
+    };
+
     return (
       <div className="App">
-        <button className="btn btn-danger">Bootstrap Test</button>
+        <NavBar authed={authed} />
+        {loadComponent()}
       </div>
     );
   }
